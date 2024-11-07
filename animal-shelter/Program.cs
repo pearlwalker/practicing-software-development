1eeb9dcb29559ea3704179f4b4b632ed01187bae
Gavin <111817327+gavinwebst@users.noreply.github.com>
2024-11-07 09:02:25 -0500

    Template for the hw
    super simple and basic template and anything that needs to be changed can be changed

---
+++ animal-shelter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+
+namespace animal_shelter
+{
+
+
+
+    internal class Program
+    {
+        
+        
+
+        //the dog class or the general pet class 
+        class Pet
+        {
+            public int age;
+            public string name;
+            public string breed;
+
+            //Pet constructor
+           
+
+        }
+
+        //the person class 
+        class Person()
+        {
+            public int age;
+            public string name;
+
+            //person constructor
+          
+
+        }
+
+        //Making an empty dictionary wiht the name as the key to the Pet object
+        Dictionary<string, object> animalList = new Dictionary<string, object>();
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
